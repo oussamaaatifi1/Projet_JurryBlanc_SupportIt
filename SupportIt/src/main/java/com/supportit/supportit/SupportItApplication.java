fff2ed7126794d8f0c67729d98b003f3af4adeda
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-05 16:41:20 +0100

    créer projet et ajouter dependencies et structure projet

---
+++ SupportIt/src/main/java/com/supportit/supportit/SupportItApplication.java
@@ -0,0 +1,13 @@
+package com.supportit.supportit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportItApplication.class, args);
+    }
+
+}
