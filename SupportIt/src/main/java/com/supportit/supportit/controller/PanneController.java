eb12809e0844cc34efacbfc9e791f183f7cc2cfc
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-07 18:06:45 +0100

    Créer Controller pour panne

---
+++ SupportIt/src/main/java/com/supportit/supportit/controller/PanneController.java
@@ -0,0 +1,36 @@
+package com.supportit.supportit.controller;
+
+import com.supportit.supportit.entity.Panne;
+import com.supportit.supportit.service.PanneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/panne")
+public class PanneController {
+
+    @Autowired
+    PanneService panneService;
+
+    @PostMapping("/add")
+    public Panne addPanne(@RequestBody Panne panne){
+        return panneService.addPanne(panne);
+    }
+
+    @GetMapping("/all")
+    public List<Panne> getAllPannes(){
+        return panneService.getAllPannes();
+    }
+
+    @PutMapping("/edit/{id}")
+    public Panne editPanne(@PathVariable Long id, @RequestBody Panne panne){
+        return panneService.updatePanne(id,panne);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePanne(@PathVariable Long id){
+         panneService.deletePanne(id);
+    }
+}
