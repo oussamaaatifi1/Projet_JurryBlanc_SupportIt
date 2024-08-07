acb9b73fd63a3d3fca0ebb9b9886cc861770ac46
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-07 18:04:07 +0100

    Créer Controller pour equipement

---
+++ SupportIt/src/main/java/com/supportit/supportit/controller/EquipmentController.java
@@ -0,0 +1,33 @@
+package com.supportit.supportit.controller;
+
+import com.supportit.supportit.entity.Equipment;
+import com.supportit.supportit.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/equipment")
+public class EquipmentController {
+    @Autowired
+    EquipmentService equipmentService;
+
+    @PostMapping("/add")
+    public Equipment addEquipment(@RequestBody Equipment equipment){
+        return equipmentService.addEquipment(equipment);
+    }
+    @GetMapping("/all")
+    public List<Equipment> getAllEquipment(){
+        return equipmentService.getAllEquipements();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEquipment(@PathVariable Long id){
+        equipmentService.deleteEquipment(id);
+    }
+    @PutMapping("/edit/{id}")
+    public Equipment editEquipment(@PathVariable Long id,@RequestBody Equipment equipment){
+        return equipmentService.editEquipment(id, equipment);
+    }
+}
