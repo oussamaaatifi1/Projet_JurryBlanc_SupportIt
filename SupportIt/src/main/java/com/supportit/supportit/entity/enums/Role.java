f667678ef7ac7bf4b842d526045800066ffe4e9a
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-08 09:50:37 +0100

    j'ai implementer userdetails pour role (user,admin,technicien)

--- SupportIt/src/main/java/com/supportit/supportit/entity/enums/Role.java
+++ SupportIt/src/main/java/com/supportit/supportit/entity/enums/Role.java
@@ -1,16 +1,16 @@
-//package com.supportit.supportit.entity.enums;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//
-//public enum Role implements GrantedAuthority {
-//    USER,
-//    ADMIN,
-//    TECHNICIEN;
-//
-//    @Override
-//    public String getAuthority() {
-//        return name();
-//    }
-//}
+package com.supportit.supportit.entity.enums;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER,
+    ADMIN,
+    TECHNICIEN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
