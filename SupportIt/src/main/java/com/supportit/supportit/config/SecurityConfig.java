07885609506c5dc97e012eeae7ffe974be1d2bdf
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-08 09:40:41 +0100

    j'ai ajouter configuration security

---
+++ SupportIt/src/main/java/com/supportit/supportit/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.supportit.supportit.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/api/v1/auth/registerAdmin", "/api/v1/auth/authenticate","/api/v1/auth/Admin/register").permitAll()
+                                .requestMatchers("/api/v1/auth/User/**").hasAuthority("USER")
+                                .requestMatchers("/api/v1/auth/Admin/**").hasAuthority("ADMIN")
+                                .requestMatchers("/api/v1/auth/Technicien/**").hasAuthority("TECHNICIEN")
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
