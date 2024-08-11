9b2d9cd191d226d7f8ffec3e2fdab3640fb09cf5
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-11 12:01:43 +0100

    j'ai modifier logic method editEquipement

--- SupportIt/src/main/java/com/supportit/supportit/service/impl/EquipmentServiceImpl.java
+++ SupportIt/src/main/java/com/supportit/supportit/service/impl/EquipmentServiceImpl.java
@@ -1,7 +1,7 @@
 package com.supportit.supportit.service.impl;
 
 import com.supportit.supportit.entity.Equipment;
-import com.supportit.supportit.repository.EquipmentRepository;
+import com.supportit.supportit.repository.EquipementRepository;
 import com.supportit.supportit.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,26 +12,38 @@ import java.util.List;
 public class EquipmentServiceImpl implements EquipmentService {
 
     @Autowired
-    EquipmentRepository equipmentRepository;
+    EquipementRepository equipmentRepository;
 
     @Override
     public Equipment addEquipment(Equipment equipment) {
         return equipmentRepository.save(equipment);
     }
 
-    @Override
-    public Equipment editEquipment(Long id, Equipment equipment) {
-        equipmentRepository.findById(id);
+//    @Override
+//    public Equipment editEquipment(Long id, Equipment equipment) {
+//        equipmentRepository.findById(id);
+//
+//        Equipment editEquipment = new Equipment();
+//        editEquipment.setEquipementId(id);
+//        editEquipment.setName(equipment.getName());
+//        editEquipment.setDescription(equipment.getDescription());
+//        editEquipment.setStatut(equipment.getStatut());
+//
+//
+//        return equipmentRepository.save(editEquipment);
+//    }
+@Override
+public Equipment editEquipment(Long id, Equipment equipment) {
+    return equipmentRepository.findById(id)
+            .map(existingEquipment -> {
+                existingEquipment.setName(equipment.getName());
+                existingEquipment.setDescription(equipment.getDescription());
+                existingEquipment.setStatut(equipment.getStatut());
+                return equipmentRepository.save(existingEquipment);
+            })
+            .orElseThrow(() -> new RuntimeException("Equipment not found"));
+}
 
-        Equipment editEquipment = new Equipment();
-        editEquipment.setEquipementId(id);
-        editEquipment.setName(equipment.getName());
-        editEquipment.setDescription(equipment.getDescription());
-        editEquipment.setStatut(equipment.getStatut());
-
-
-        return equipmentRepository.save(editEquipment);
-    }
 
     @Override
     public List<Equipment> getAllEquipements() {
