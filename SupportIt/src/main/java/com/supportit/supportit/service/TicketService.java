7e7eff85f309297e1162eb37c5bf6cd7dbcd64c9
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-11 11:51:30 +0100

    j'ai créer interface de service ticket et ajouter method pour implements class TicketServiceImpl

--- SupportIt/src/main/java/com/supportit/supportit/service/TicketService.java
+++ SupportIt/src/main/java/com/supportit/supportit/service/TicketService.java
@@ -8,5 +8,8 @@ import java.util.List;
 public interface TicketService {
     Ticket addTicket(Ticket ticket);
     List<Ticket> getAllTickets();
+    List<Ticket> findByTechnicien(Long id);
+    List<Ticket> findByUtilisateur(Long id);
     Ticket assignerTicket(Long id, Ticket ticket);
+    Ticket changerStatut(Long id, Ticket ticket);
 }
