858077b096c04d62b974c92960af1e3e11801237
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-11 11:53:32 +0100

    j'ai terminer class TicketServiceImpl

--- SupportIt/src/main/java/com/supportit/supportit/service/impl/TicketServiceImpl.java
+++ SupportIt/src/main/java/com/supportit/supportit/service/impl/TicketServiceImpl.java
@@ -1,6 +1,11 @@
 package com.supportit.supportit.service.impl;
 
+import com.supportit.supportit.entity.Technicien;
 import com.supportit.supportit.entity.Ticket;
+import com.supportit.supportit.entity.Utilisateur;
+import com.supportit.supportit.entity.enums.EtatTicket;
+import com.supportit.supportit.repository.PersonneRepository;
+import com.supportit.supportit.repository.TechnicienRepository;
 import com.supportit.supportit.repository.TicketRepository;
 import com.supportit.supportit.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +19,17 @@ public class TicketServiceImpl implements TicketService {
 
     @Autowired
     TicketRepository ticketRepository;
+    @Autowired
+    TechnicienRepository technicienRepository;
+    @Autowired
+    private PersonneRepository personneRepository;
 
     @Override
     public Ticket addTicket(Ticket ticket) {
+        Utilisateur utilisateur = (Utilisateur) personneRepository.findById(ticket.getUtilisateur().getId())
+                .orElseThrow(() -> new RuntimeException("Utilisateur not found"));
+        ticket.setStatut(EtatTicket.NOTRAITE);
+        ticket.setUtilisateur(utilisateur);
         ticket.setDateDeCreation(LocalDate.now());
         return ticketRepository.save(ticket);
     }
@@ -27,13 +40,44 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
+    public List<Ticket> findByTechnicien(Long id) {
+        Technicien technicien = (Technicien) personneRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Technicien not found"));
+        List<Ticket> tickets = ticketRepository.findAllByTechnicien(technicien);
+        return tickets;
+    }
+
+    @Override
+    public List<Ticket> findByUtilisateur(Long id) {
+        Utilisateur utilisateur = (Utilisateur) personneRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Utilisateur not found"));
+        List<Ticket> tickets = ticketRepository.findAllByUtilisateur(utilisateur);
+        return tickets;
+    }
+
+
+    @Override
     public Ticket assignerTicket(Long id, Ticket ticket) {
-        ticketRepository.findById(id);
 
-        Ticket assignerTicked = new Ticket();
+        Ticket assignerTicked = ticketRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ticket not found"));
 
-        assignerTicked.setTechnicien(ticket.getTechnicien());
+        Technicien technicien = (Technicien) personneRepository.findById(ticket.getTechnicien().getId())
+                .orElseThrow(() -> new RuntimeException("Technicien not found"));
+        assignerTicked.setTechnicien(technicien);
+
 
         return ticketRepository.save(assignerTicked);
+    }
+
+    @Override
+    public Ticket changerStatut(Long id, Ticket ticket) {
+
+        Ticket ticketStaut = ticketRepository.findById(id)
+                .orElseThrow(() ->new RuntimeException("Ticket not found"));
+
+        ticketStaut.setStatut(ticket.getStatut());
+
+        return ticketRepository.save(ticketStaut);
     }
 }
