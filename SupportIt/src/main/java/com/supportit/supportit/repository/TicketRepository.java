9d91ef6bb53e0abaf4851d9067fcaf81f2b83b14
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-11 11:49:27 +0100

    j'ai ajouter des method dans repository de ticket pour rehercher id de technicien et utilisateur

--- SupportIt/src/main/java/com/supportit/supportit/repository/TicketRepository.java
+++ SupportIt/src/main/java/com/supportit/supportit/repository/TicketRepository.java
@@ -1,8 +1,14 @@
 package com.supportit.supportit.repository;
 
 
+import com.supportit.supportit.entity.Technicien;
 import com.supportit.supportit.entity.Ticket;
+import com.supportit.supportit.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findAllByUtilisateur(Utilisateur utilisateur);
+    List<Ticket> findAllByTechnicien(Technicien technicien);
 }
