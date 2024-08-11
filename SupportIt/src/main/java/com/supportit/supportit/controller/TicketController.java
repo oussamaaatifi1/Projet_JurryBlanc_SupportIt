186c849eeba94502e66d4ad847baf994ee56d04c
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-11 12:06:39 +0100

    aussi modifier dans requette http path pour authorisation dans Class SecurityCOnfig

--- SupportIt/src/main/java/com/supportit/supportit/controller/TicketController.java
+++ SupportIt/src/main/java/com/supportit/supportit/controller/TicketController.java
@@ -8,25 +8,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/auth/tickets")
 public class TicketController {
 
     @Autowired
     TicketService ticketService;
 
-    @PostMapping("/User/tickets/add")
+    @PostMapping("/add")
     public Ticket addTicket(@RequestBody Ticket ticket){
         return ticketService.addTicket(ticket);
     }
 
-    @GetMapping("/User/tickets/all")
+    @GetMapping("/all")
     public List<Ticket> getAllTickets(){
         return ticketService.getAllTickets();
     }
 
-    @PutMapping("/Admin/tickets/assigner/{id}")
+    @GetMapping("/tickets/{id}")
+    public List<Ticket> ticketsByTechnicien(@PathVariable Long id){
+        return ticketService.findByTechnicien(id);
+    }
+
+    @GetMapping("/mytickets/{id}")
+    public List<Ticket> ticketsByUtilisateur(@PathVariable Long id){
+        return ticketService.findByUtilisateur(id);
+    }
+
+    @PutMapping("/assigner/{id}")
     public Ticket assignerTicket(@PathVariable Long id, @RequestBody Ticket ticket){
         return ticketService.assignerTicket(id, ticket);
+    }
+
+    @PutMapping("/statut/{id}")
+    public Ticket changetStatut(@PathVariable Long id, @RequestBody Ticket ticket){
+        return ticketService.changerStatut(id, ticket);
     }
 
 }
