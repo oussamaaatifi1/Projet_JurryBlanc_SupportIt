1e91150e13abe0bacc50c9ccc19478682bc05119
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-14 01:47:40 +0100

    j'ai configurer SecurityConfig avec authorisation avec les roles

--- SupportIt/src/main/java/com/supportit/supportit/config/SecurityConfig.java
+++ SupportIt/src/main/java/com/supportit/supportit/config/SecurityConfig.java
@@ -24,10 +24,12 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
 //                                .requestMatchers("/api/v1/auth/**").permitAll()
-                                .requestMatchers("/api/v1/auth/authenticate","/api/v1/auth/register/registerAdmin").permitAll()
+                                .requestMatchers("/api/v1/auth/authenticate","/api/v1/auth/register/user").permitAll()
                                 .requestMatchers("/api/v1/auth/tickets/all","/api/v1/auth/tickets/add").hasAuthority("USER")
-                                .requestMatchers("/api/v1/auth/Admin/equipements/**","/api/v1/auth/Admin/panne/**","/api/v1/auth/assigner/**","/api/v1/auth/register/**").hasAuthority("ADMIN")
-                                .requestMatchers("/api/v1/auth/tickets/all").hasAuthority("TECHNICIEN")
+                                .requestMatchers("/api/v1/auth/Admin/equipements/add/**","/api/v1/auth/Admin/equipements/edit/**","/api/v1/auth/Admin/equipements/delete/**","/api/v1/auth/Admin/equipements/delete/**","/api/v1/auth/Admin/equipements/statut/**","/api/v1/auth/assigner/**",
+                                        "/api/v1/auth/register/**","/api/techniciens",
+                                        "/api/v1/auth/Admin/panne/add/**","/api/v1/auth/Admin/panne/edit/**","/api/v1/auth/Admin/panne/delete/**","/api/v1/auth/tickets/all").hasAuthority("ADMIN")
+                                .requestMatchers("/api/v1/auth/tickets/all","/api/v1/auth/tickets/mytickets/**").hasAuthority("TECHNICIEN")
                                 .anyRequest()
                                 .authenticated()
                 )
