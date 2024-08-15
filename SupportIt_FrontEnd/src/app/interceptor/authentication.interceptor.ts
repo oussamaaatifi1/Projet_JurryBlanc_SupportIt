b0e15da7aeeb61670b11aaa9856fc14eeecb2a17
oussamaaatifi1 <nisowpro@gmail.com>
2024-08-15 11:59:03 +0100

    j'ai créer guard pour authentification with token

---
+++ SupportIt_FrontEnd/src/app/interceptor/authentication.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthentificationService } from '../service/authentification.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthentificationService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.authService.getToken();
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
